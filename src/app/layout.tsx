4c458cb0494f82db5dc44f11e900043b59e12449
chirag jain <chiragjainrockcool@gmail.com>
2025-12-24 19:36:06 +0000

    5 cm right shift kr other the header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className="antialiased bg-background text-foreground">
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">
+          <main className="flex-grow pl-48">
             {children}
           </main>
           <Footer />
