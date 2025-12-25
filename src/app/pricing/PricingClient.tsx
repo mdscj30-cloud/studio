b9ea9cf03e85941005fe02bce9c7fe9163ffdab3
chirag jain <chiragjainrockcool@gmail.com>
2025-12-25 19:08:53 +0000

    insteed of calander link we will align 30 min meeting booking same as ca

--- src/app/pricing/PricingClient.tsx
+++ src/app/pricing/PricingClient.tsx
@@ -112,7 +112,7 @@ export function PricingClient() {
                       </CardContent>
                       <CardFooter>
                         <Button asChild className="w-full" variant={recommendedPlan === plan.title ? 'accent' : 'default'} size="lg">
-                          <Link href="https://calendar.google.com/" target="_blank">Book a Discovery Call</Link>
+                          <Link href="https://calendly.com/nconsulting/30min" target="_blank">Book a Discovery Call</Link>
                         </Button>
                       </CardFooter>
                     </Card>
